cd43185edda5e0a75f2cc1ccb066b96e02e779dc
vodinhtuan <vodinhtuan174@gmail.com>
2022-08-12 18:34:50 +0700

    update

--- Module_03/ss02_Design_And_Create_Database/homeworks/quanLyBanHang.sql
+++ Module_03/ss02_Design_And_Create_Database/homeworks/quanLyBanHang.sql
@@ -5,6 +5,7 @@ CREATE TABLE Customer(
     cName VARCHAR(50),
     cAGE INT
 );
+
 CREATE TABLE `Order`(
 	oID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     cID INT,
@@ -13,3 +14,17 @@ CREATE TABLE `Order`(
     FOREIGN KEY (cID) REFERENCES Customer (cID)
 );
 
+CREATE TABLE OrderDetail(
+	oID INT,
+    pID INT,
+    odQTY VARCHAR(100),
+    PRIMARY KEY (oID, pID),
+    FOREIGN KEY (oID) REFERENCES `Order` (oID),
+    FOREIGN KEY (pID) REFERENCES Product (pID)
+);
+
+CREATE TABLE Product(
+    pID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    pName VARCHAR(50),
+    pPrice FLOAT
+);
