43535b72f10c68db90448fcebba4be42d2eada66
vodinhtuan <vodinhtuan174@gmail.com>
2022-08-26 17:01:36 +0700

    update

--- Module_03/casestudy01/case_study.sql
+++ Module_03/casestudy01/case_study.sql
@@ -219,18 +219,20 @@ VALUE(1,  2, 4, 5),
 SELECT * FROM hop_dong_chi_tiet;
 
 
--- task 2
+-- task 2 	Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” 
+		-- hoặc “K” và có tối đa 15 kí tự.
 select * 
 from nhan_vien 
 where (ho_ten like 'H%' or ho_ten like 'T%' or ho_ten like 'K%') and (char_length(ho_ten)<=15);
 
--- task 3
+-- task 3 Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
 select *
 from khach_hang
 where (dia_chi like '%Quảng Trị' or dia_chi like '%Đà Nẵng') and year(curdate())-year(ngay_sinh) between 18 and 50;
 select * from khach_hang;
 
--- task 4
+-- task 4   	Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. Kết quả hiển thị được sắp xếp tăng dần 
+		 -- theo số lần đặt phòng của khách hàng. Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
 select k.ma_khach_hang , k.ho_ten , l.ten_loai_khach , h.ma_hop_dong , count(h.ma_khach_hang) as so_lan_dat_phong
 from khach_hang k 
 join loai_khach l on k.ma_loai_khach = l.ma_loai_khach
@@ -239,7 +241,10 @@ where l.ten_loai_khach ='Diamond'
 group by h.ma_khach_hang
 order by so_lan_dat_phong;
 
--- task 5
+/* task 5 Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc,
+ tong_tien (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ 
+ bảng dich_vu_di_kem, hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng.
+ (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra). */
 create view w_tinh_tien as 
 select k.ma_khach_hang, k.ho_ten , lk.ten_loai_khach ,hd.ma_hop_dong ,dv.ten_dich_vu, lk.ma_loai_khach,
 hd.ngay_lam_hop_dong, hd.ngay_ket_thuc,( ifnull(dv.chi_phi_thue,0)+sum(ifnull(hdct.so_luong,0)*ifnull(dvdk.gia,0))) as tong_tien
@@ -255,7 +260,9 @@ order by k.ma_khach_hang;
 select * 
 from w_tinh_tien;
 
--- task 6 
+/*  task 6 	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ 
+			chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+*/
 select dv.ma_dich_vu,dv.ten_dich_vu,dv.dien_tich,dv.chi_phi_thue ,ldv.ten_loai_dich_vu
 from dich_vu dv 
 join loai_dich_vu ldv on ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
@@ -266,7 +273,9 @@ from hop_dong hd
 where  (year(hd.ngay_lam_hop_dong) =2021) and month(hd.ngay_lam_hop_dong) in(1,2,3))
 group by dv.dien_tich;
 
--- task 7
+/* task 7 Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của 
+tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
+*/
 select dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich,
 dv.so_nguoi_toi_da, dv.chi_phi_thue, ldv.ten_loai_dich_vu
 from dich_vu dv
@@ -278,3 +287,69 @@ select hd.ma_dich_vu
 from hop_dong hd 
 where year(hd.ngay_lam_hop_dong)=2021)
 group by hd.ma_dich_vu;
+
+-- task 8  	Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+-- Học viên sử dụng theo 3 cách khác nhau để thực hiện yêu cầu trên.
+
+select distinct k.ho_ten   -- DISTINCT trong SQL được dùng để lấy giá trị không trùng lặp
+from khach_hang k;
+
+select k.ho_ten
+from khach_hang k
+group by k.ho_ten;
+
+select k.ho_ten
+from khach_hang k
+union
+select k.ho_ten
+from khach_hang k;
+
+/* task 9 	Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 
+			thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+*/
+select month(ngay_lam_hop_dong) as thang ,count(ma_khach_hang) as so_luong_dat_phong
+from hop_dong
+where year(ngay_lam_hop_dong) = 2021
+group by thang;
+/* task 10:	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. 
+			Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem 
+			(được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+*/
+select hd.ma_hop_dong, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, hd.tien_dat_coc,
+ifnull(sum(hdct.so_luong),0) as so_luong
+from hop_dong hd 
+left join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+left join dich_vu_di_kem  dvdk on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+group by hd.ma_hop_dong
+order by hd.ma_hop_dong;
+/* task 11: Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” và 
+			có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
+*/
+select dvdk.ma_dich_vu_di_kem,dvdk.ten_dich_vu_di_kem
+from dich_vu_di_kem dvdk 
+join hop_dong_chi_tiet hdct on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+join hop_dong hd on hd.ma_hop_dong = hdct.ma_hop_dong
+join khach_hang kh on kh.ma_khach_hang = hd.ma_khach_hang
+join loai_khach lk on lk.ma_loai_khach = kh.ma_loai_khach
+where kh.ma_loai_khach = 1 and (kh.dia_chi like '%Vinh' or kh.dia_chi like '%Quảng Ngãi');
+
+/* task 12: Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), 
+	ten_dich_vu, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), tien_dat_coc 
+    của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng
+    đặt vào 6 tháng đầu năm 2021.
+*/
+select  hd.ma_hop_dong , nv.ho_ten as ten_nhan_vien, kh.ho_ten as ten_khach_hang ,kh.so_dien_thoai ,dv.ma_dich_vu, dv.ten_dich_vu ,
+sum(ifnull(hdct.so_luong,0)) as so_luong_dich_vu_di_kem, hd.tien_dat_coc
+from hop_dong hd 
+left join nhan_vien nv on nv.ma_nhan_vien = hd.ma_nhan_vien
+left join khach_hang kh on kh.ma_khach_hang = hd.ma_khach_hang
+left join dich_vu dv on dv.ma_dich_vu = hd.ma_dich_vu
+left join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong
+where (year(hd.ngay_lam_hop_dong) =2020 and (month(hd.ngay_lam_hop_dong) in (10,11,12)))
+and hd.ma_hop_dong not in (
+select hd.ma_hop_dong
+from hop_dong hd 
+where year(hd.ngay_lam_hop_dong) =2021 and (month(hd.ngay_lam_hop_dong) between 1 and 6))
+group by hd.ma_hop_dong;
+
+
