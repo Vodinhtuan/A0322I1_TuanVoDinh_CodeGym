126de8747eb4be8a36f169fb2e49902e9f784075
vodinhtuan <vodinhtuan174@gmail.com>
2022-11-04 23:23:52 +0700

    update

--- Module_03/Test2/src/main/resources/test2.sql
+++ Module_03/Test2/src/main/resources/test2.sql
@@ -8,9 +8,13 @@ create table thanhToan
 create table thueTro
 (
 	maPhong int primary key auto_increment,
-    name varchar(30),
+    name varchar(50),
     phone int,
     ngayThue date,
+    ghiChu varchar(200),
     maThanhToan int not null,
     foreign key (maThanhToan) references thanhToan(maThanhToan)
 );
+select * from thanhToan;
+delete from thueTro where maPhong = 1;
+select tt.maPhong, tt.name, tt.phone, tt.ngayThue, tt.ghiChu, th.maThanhToan from thanhToan th join thueTro tt on th.maThanhToan = th.maThanhToan;
--- Module_03/demoFinalTest/src/main/resources/finalModule3.sql
+++ Module_03/demoFinalTest/src/main/resources/finalModule3.sql
@@ -1,1 +1,18 @@
 create database finalModule3;
+use finalModule3;
+create table category
+(
+	category_id int primary key auto_increment,
+    name_category varchar(30)
+);
+create table product
+(
+	product_id int primary key auto_increment,
+    name varchar(50),
+    price double,
+    quantity int,
+    color varchar(50),
+    `description` varchar(200),
+    category_id int not null,
+    foreign key (category_id) references category(category_id)
+);
